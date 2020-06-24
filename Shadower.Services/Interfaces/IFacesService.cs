d447d6df7730fd045d42bd00638bf6f7ba426f7d
Owliie <sladurcheto5232@abv.bg>
2020-06-25 00:08:13 +0300

    Added faces service and controller structure

---
+++ Shadower.Services/Interfaces/IFacesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shadower.Data.Models;
+
+namespace Shadower.Services.Interfaces
+{
+    public interface IFacesService
+    {
+        List<Face> GetFound();
+    }
+}
