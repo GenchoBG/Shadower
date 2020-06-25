2ce1b17a58e4499d075779d7398fca6faff8522a
GenchoBG <genchobg11@gmail.com>
2020-06-25 21:10:22 +0300

    Fix bugs with real-time notifications

--- Shadower.Web/wwwroot/js/notifications.js
+++ Shadower.Web/wwwroot/js/notifications.js
@@ -3,6 +3,10 @@
 $(document).ready(function() {
     connection = new signalR.HubConnectionBuilder().withUrl("/Notifications").build();
 
+    connection.start().catch(function (err) {
+        return console.error(err.toString());
+    });
+
     connection.on("UpdateFoundFaces", function (date, link) {
         addPost({ uploadedDateTime: date, link: link });
     });
